a9e9ec1b5aa5fa91666c4db39e5c85f3ddbb0bce
Yousuf Kalim <yousufkalim@outlook.com>
2023-03-10 18:35:17 +0500

    Removed admin

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -8,52 +8,6 @@ import IRequest from '@interfaces/request.interface';
 import { JWT_SECRET } from '@config';
 
 // Check auth
-export const checkAdminAuth = (
-  req: IRequest,
-  res: Response,
-  next: NextFunction,
-): Response | void => {
-  // Get token from Header
-  const header = req.get('Authorization');
-
-  // Check if not token
-  if (!header?.startsWith('Bearer')) {
-    return res.status(403).json({
-      success: false,
-      message: 'No token found, Authorization denied',
-    });
-  }
-
-  try {
-    // Decrypting token
-    const token = header.split(' ')[1];
-    const { user } = jwt.verify(token, JWT_SECRET) as JwtPayload;
-
-    if (!user || (user.role !== 'admin' && user.role !== 'superadmin')) {
-      return res.status(403).json({
-        success: false,
-        message: 'You are not authorized to access this resource',
-      });
-    }
-
-    req.user = user;
-    req.token = token;
-
-    // If user authenticated
-    res.set(
-      'cache-control',
-      'no-cache, no-store, private, must-revalidate, post-check=0, pre-check=0',
-    );
-
-    // Done
-    next();
-  } catch (err) {
-    // If not
-    res.status(403).json({ success: false, message: 'Your session has been expired' });
-  }
-};
-
-// Check auth
 export const checkUserAuth = (
   req: IRequest,
   res: Response,
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,13 +3,11 @@
  * @author Yousuf Kalim
  */
 import { Router } from 'express';
-import adminRoutes from './admins.route';
 import userRoutes from './users.route';
 import authRoutes from './auth.route';
 const router = Router();
 
 // Parent Routes
-router.use('/admins', adminRoutes); // All the user routes
 router.use('/users', userRoutes); // All the user routes
 router.use('/auth', authRoutes); // All the auth routes
 
