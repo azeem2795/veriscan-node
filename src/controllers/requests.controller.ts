15a2ebc475c2e4d17431db5ddda245bce5f56caa
Yousuf Kalim <yousufkalim@outlook.com>
2023-03-13 16:29:35 +0500

    Code request CRUD apis

--- src/controllers/requests.controller.ts
+++ src/controllers/requests.controller.ts
@@ -27,6 +27,59 @@ export const create = async (req: IRequest, res: Response): Promise<Response> =>
 };
 
 /**
+ * get all requests
+ * @param {object} req
+ * @param {object} res
+ */
+export const getAllRequests = async (req: IRequest, res: Response): Promise<Response> => {
+  try {
+    if (req.user?.role === 'admin') {
+      const requests = await Requests.find().populate('brand').sort({ createdAt: -1 });
+
+      return res.json({ success: true, requests });
+    } else {
+      const requests = await Requests.find({ brand: req.user?._id }).sort({ createdAt: -1 });
+
+      return res.json({ success: true, requests });
+    }
+  } catch (err) {
+    // Error handling
+    // eslint-disable-next-line no-console
+    console.log('Error ----> ', err);
+    return res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+};
+
+/**
+ * Get request by id
+ * @param {object} req
+ * @param {object} res
+ */
+export const getRequestById = async (req: IRequest, res: Response): Promise<Response> => {
+  const { id } = req.params;
+  try {
+    const request = await Requests.findById(id).populate('brand');
+
+    if (
+      req.user?.role === 'brand' &&
+      typeof request?.brand === 'object' &&
+      req.user?._id !== String(request?.brand?._id)
+    ) {
+      return res
+        .status(401)
+        .json({ success: false, message: 'You are not authorized to access this resource' });
+    }
+
+    return res.json({ success: true, request });
+  } catch (err) {
+    // Error handling
+    // eslint-disable-next-line no-console
+    console.log('Error ----> ', err);
+    return res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+};
+
+/**
  * delete request
  * @param {object} req
  * @param {object} res
@@ -49,6 +102,38 @@ export const deleteRequest = async (req: IRequest, res: Response): Promise<Respo
     await Requests.findByIdAndDelete(id);
 
     return res.json({ success: true, message: 'Request has been deleted' });
+  } catch (err) {
+    // Error handling
+    // eslint-disable-next-line no-console
+    console.log('Error ----> ', err);
+    return res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+};
+
+/**
+ * Reject request
+ * @param {object} req
+ * @param {object} res
+ */
+export const rejectRequest = async (req: IRequest, res: Response): Promise<Response> => {
+  const { id } = req.params;
+  try {
+    const request = await Requests.findById(id);
+
+    if (!request) {
+      return res.status(404).json({ success: false, message: 'Request not found' });
+    }
+
+    if (request.status === 'approved') {
+      return res
+        .status(400)
+        .json({ success: false, message: 'You cannot decline processed request' });
+    }
+
+    request.status = 'rejected';
+    await request.save();
+
+    return res.json({ success: true, message: 'Request has been rejected', request });
   } catch (err) {
     // Error handling
     // eslint-disable-next-line no-console
--- src/routes/requests.route.ts
+++ src/routes/requests.route.ts
@@ -3,8 +3,14 @@
  * @author Yousuf Kalim
  */
 import { Router } from 'express';
-import { checkUserAuth } from '@middleware/auth.middleware';
-import { create, deleteRequest } from '@controllers/requests.controller';
+import { checkAdminAuth, checkUserAuth } from '@middleware/auth.middleware';
+import {
+  create,
+  deleteRequest,
+  rejectRequest,
+  getAllRequests,
+  getRequestById,
+} from '@controllers/requests.controller';
 import { validateCodeRequest, isValidated } from '@middleware/validations.middleware';
 
 const router = Router();
@@ -12,9 +18,18 @@ const router = Router();
 /**
  * ////////////////////////// Routes /////////////////////////
  * @method post create a request
+ * @method get get all request
+ * @method get get request by id
+ * @method patch reject the request
  * @method delete delete a request
  */
 router.post('/', checkUserAuth, validateCodeRequest, isValidated, create);
+
+router.get('/', checkUserAuth, getAllRequests);
+router.get('/:id', checkUserAuth, getRequestById);
+
+router.patch('/reject/:id', checkAdminAuth, rejectRequest);
+
 router.delete('/:id', checkUserAuth, deleteRequest);
 
 // Export
