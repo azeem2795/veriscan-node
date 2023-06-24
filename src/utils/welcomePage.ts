73a3e7d3b0d624e1d07bb4ff98141753d7969aff
connor-vpw <135121511+connor-vpw@users.noreply.github.com>
2023-06-24 16:18:19 -0700

    Update welcomePage.ts
    undid previous changes

--- src/utils/welcomePage.ts
+++ src/utils/welcomePage.ts
@@ -1,39 +1,32 @@
-/**
- * Welcome page for the application.
- * @author Yousuf Kalim
- */
-import { APP_NAME } from '@config';
-
 const welcomePage = `<!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome to the VeriScan API</title>
+    <title>Welcome</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
     <style>
         * {
             padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-        }
-
-        body {
-            margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-            'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-        }
-
-        .Hero {
-            text-align: center;
-        }
-
+@@ -30,18 +29,22 @@ const welcomePage = `<!DOCTYPE html>
         .Hero-logo {
             height: 40vmin;
+            pointer-events: none;
             margin-bottom: 10px;
-            filter: hue-rotate(110deg);
-            animation: Hero-logo-spin infinite 20s linear;
         }
-
+        @media (prefers-reduced-motion: no-preference) {
+            .Hero-logo {
+                animation: Hero-logo-spin infinite 20s linear;
+            }
+        }
+        .Hero-header {
+            background-color: #282c34;
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+@@ -53,16 +56,17 @@ const welcomePage = `<!DOCTYPE html>
+        .Hero-header p {
+            font-size: 20px;
+            line-height: 2.5;
+        }
         @keyframes Hero-logo-spin {
             from {
                 transform: rotate(0deg);
@@ -42,48 +35,19 @@ const welcomePage = `<!DOCTYPE html>
                 transform: rotate(360deg);
             }
         }
-
-        .Hero-header {
-            background-color: #0080ff;
-            min-height: 100vh;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            font-size: calc(10px + 2vmin);
-            color: white;
-        }
-
-        .Hero-header p {
-            font-size: 20px;
-            line-height: 1.5;
-            margin-bottom: 10px;
-        }
-
-        .Hero-header a {
-            color: white;
-            text-decoration: underline;
-        }
-
-        .Hero-header a:hover {
-            color: #80c9ff;
-        }
     </style>
 </head>
-<body>
-<div class="app">
+@@ -71,13 +75,15 @@ const welcomePage = `<!DOCTYPE html>
     <div class="Hero">
         <header class="Hero-header">
             <img src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png" class="Hero-logo" alt="logo" />
-            <h4>Welcome to VeriScan</h4>
-            <p>This is the main route of VeriScan.</p>
-            <p>If you are here by mistake, please visit <a href="https://getveriscan.com">getveriscan.com</a> for more information.</p>
+            <h4>Welcome to ${APP_NAME}</h4>
+            <p>This is the main route of the application.</p>
         </header>
     </div>
 </div>
 </body>
 </html>
-
 `;
 
 export default welcomePage;
